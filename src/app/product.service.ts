642b9277a051243051398e99010b6bc13f524a0a
RaphaelYervantian <raphael.yervantian@daf.qld.gov.au>
2020-11-18 15:45:43 +1000

    completed part3
    completed part 3 but rsjx synax out of date

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
@@ -9,6 +9,7 @@ export class ProductService {
   constructor( private _http: Http ) { }
 
   getAlbum(id:number){
+    return this._http.get(this._albumUrl).pipe(map(response => <album>response.json()));
 
   }
 
